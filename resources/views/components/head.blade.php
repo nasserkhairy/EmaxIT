80cbb2b1d71fc5199a6698f317738288b0f394c5
Omnia-Abdelhamed <omnia.silmy@gmail.com>
2025-05-26 20:34:19 +0300

    handle seo

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,6 +1,14 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
+<meta charset="UTF-8" />
+<title>EmaxIT International Inc.</title>
+<meta name='robots' content='max-image-preview:large' />
+<meta name="keywords" content="it solutions,workforce management,business intelligence,custom software development,digital transformation,emaxit international,it consulting,enterprise solutions,technology innovation"/>
+<meta name="description" content="EmaxIT International Inc. offers cutting-edge IT solutions, including workforce management (iVisionMT) and business intelligence (XightStudio). Explore custom software development, AI-driven analytics, cloud solutions, and more."/>
+<link rel="alternate" hreflang="en-US" href="https://emaxit-production.up.railway.app/"/>
+<meta name="format-detection" content="telephone=no">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="shortcut icon" href="https://emaxit.com/wp-content/uploads/favicon.ico#295" type="image/x-icon" />
+{{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
+
 <meta name="author" content="">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <!-- حط دا في <head> -->
