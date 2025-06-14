7555809c875af12fe335de3fda630a477470e640
Omnia-Abdelhamed <omnia.silmy@gmail.com>
2025-06-14 14:11:07 +0300

    last css minimize

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -19,6 +19,8 @@
 <!-- Additional CSS Files -->
 <link rel="stylesheet" href="{{asset('assets/css/fontawesome.css')}}">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/css/templatemo-seo-dream.css')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/custom.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+    crossorigin="anonymous" />
+<link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
 <link rel="stylesheet" href="{{ asset("assets/bootstrap-icons/bootstrap-icons.css") }}">
